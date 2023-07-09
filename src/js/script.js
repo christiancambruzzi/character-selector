ddd47b5c9e9a798c869eba7111f8afcb785c6f1a
Christian Cambruzzi <christiancambruzzi@gmail.com>
2023-07-09 01:31:58 -0300

    refatoração

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,35 @@ const characters = document.querySelectorAll(".character");
 
 characters.forEach((character) => {
   character.addEventListener("mouseenter", () => {
-    const selectedCharacter = document.querySelector(".selected");
-    selectedCharacter.classList.remove("selected");
+    if (window.innerWidth < 450) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+
+    CharacterSelectionRemove();
     character.classList.add("selected");
-
-    const characterBigImage = document.querySelector(".big-character");
-
-    const characterId = character.attributes.id.value
-
-    characterBigImage.src = `./src/imagens/card-${characterId}.png`;
+    modifySelectedCharacterImage(character);
+    modifySelectedCharacterName(character);
+    modifySelectedCharacterDescription(character);
   });
 });
+
+function CharacterSelectionRemove() {
+  const selectedCharacter = document.querySelector(".selected");
+  selectedCharacter.classList.remove("selected");
+}
+
+function modifySelectedCharacterImage(character) {
+  const characterBigImage = document.querySelector(".big-character");
+  const characterId = character.attributes.id.value;
+  characterBigImage.src = `./src/imagens/card-${characterId}.png`;
+}
+
+function modifySelectedCharacterName(character) {
+  const characterName = document.getElementById("character-name");
+  characterName.innerText = character.getAttribute("data-name");
+}
+
+function modifySelectedCharacterDescription(character) {
+  const characterDescription = document.getElementById("character-description");
+  characterDescription.innerText = character.getAttribute("data-description");
+}
